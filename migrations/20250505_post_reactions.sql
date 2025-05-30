02976b6ca8623f53b1b2df93ebc2667d054a6fa8
helouazizi <ouazizi2code@gmail.com>
2025-05-30 14:14:36 +0100

    new  verion

--- migrations/20250505_post_reactions.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS post_reactions (
-    user_id INTEGER,
-    post_id INTEGER,
-    reaction TEXT CHECK(reaction IN ('like', 'dislike')),
-    PRIMARY KEY (user_id, post_id)
-);
